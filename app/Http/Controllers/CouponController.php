ec936bfa1d37d2206a8e2cbf6528d426888e9d47
Vivek Chandra Ojha <vivek.chandra.ojha@gmail.com>
2022-06-12 01:28:07 +0545

    complex logic apply coupon

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Coupon;
 use Illuminate\Http\Request;
+use App\Models\AppliedCoupon;
+use Illuminate\Support\Carbon;
 use App\Http\Requests\AddCoupenRequest;
 use App\Http\Requests\ApplyCouponRequest;
 use App\Http\Requests\DeleteCouponRequest;
@@ -38,18 +40,29 @@ class CouponController extends Controller
     public function applyCoupon(ApplyCouponRequest $request){
       $input = $request->validated();
       $coupon = Coupon::where('coupon_code', '=', $input['coupon_code'])->get();
+      $nowTime = date('Y-m-d H:i:s');
+      $response = array();
+      $code = 201;
       if(sizeof($coupon) == 1){
         $coupon = $coupon[0];
-        //if($coupon->expiry_date > date(''))
+        if($coupon->expiry_date > $nowTime){
         if($coupon->type == "percentdiscount"){
           $this->discountAmount = $coupon->discount*$input['price']/100;
         } else {
           $this->discountAmount = $coupon->discount; 
         }
+        $response = ["data"=>array("status"=>"Success","price"=>array("original_price"=>$input['price'],"discount"=>$this->discountAmount))];
+        } else {
+            $response = ["data"=>array("status"=>"Coupon Expired!")];
+            $code = 410;    
+        }
       } else {
-        return response()->json(["data"=>array("status"=>"Coupon Code Not Found")],404);
+        $response = ["data"=>array("status"=>"Coupon Code Not Found!")];
+        $code = 404;
       }
-      
+      $couponLog = array("coupon_code"=>$input['coupon_code'],"request"=>json_encode($input),"response"=>json_encode($response));
+      AppliedCoupon::create($couponLog);      
+      return response()->json($response,$code);      
     }
     
 }
